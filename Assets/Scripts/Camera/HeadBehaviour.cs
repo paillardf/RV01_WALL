b47f92fcb23047175845bc19a08ce84855b0faad
Maerig <jean-Loup.roussel-clouet@etu.utc.fr>
2013-11-22 23:58:01 +0100

    Corrige l'orientation de la tête

--- Assets/Scripts/Camera/HeadBehaviour.cs
+++ Assets/Scripts/Camera/HeadBehaviour.cs
@@ -4,8 +4,12 @@ using System.Collections;
 public class HeadBehaviour : MonoBehaviour {
 
 	public Camera camera;
-
+	
 	void LateUpdate () {
-		transform.rotation = camera.transform.rotation;
+		Debug.DrawRay(camera.transform.position, camera.transform.forward, Color.red);
+		Debug.DrawRay(transform.position, transform.forward, Color.green);
+		transform.localRotation = camera.transform.localRotation;
+		// Fixes the head orientation
+		transform.localRotation = new Quaternion(-transform.localRotation.y, transform.localRotation.z, -transform.localRotation.x, transform.localRotation.w);
 	}
 }
