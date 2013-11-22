5659efef72135b7bbd3611f22ace28f67dad6639
Maerig <jean-Loup.roussel-clouet@etu.utc.fr>
2013-11-23 00:09:45 +0100

    Met à jour le prefab

--- Assets/Scripts/Camera/HeadBehaviour.cs
+++ Assets/Scripts/Camera/HeadBehaviour.cs
@@ -6,8 +6,6 @@ public class HeadBehaviour : MonoBehaviour {
 	public Camera camera;
 	
 	void LateUpdate () {
-		Debug.DrawRay(camera.transform.position, camera.transform.forward, Color.red);
-		Debug.DrawRay(transform.position, transform.forward, Color.green);
 		transform.localRotation = camera.transform.localRotation;
 		// Fixes the head orientation
 		transform.localRotation = new Quaternion(-transform.localRotation.y, transform.localRotation.z, -transform.localRotation.x, transform.localRotation.w);
