bc3f72785b33d62b9a37504835bfbf3ca6742196
Maerig <jean-Loup.roussel-clouet@etu.utc.fr>
2013-11-29 18:44:33 +0100

    Met à jour les prefabs

--- Assets/Scripts/Build/DoorBehaviour.cs
+++ Assets/Scripts/Build/DoorBehaviour.cs
@@ -2,25 +2,26 @@
 using System.Collections;
 
 public class DoorBehaviour : MonoBehaviour {
-	public  float OpenSpeed = 3;
-	private bool  isOpened = false;
-	private bool  isBroken = false;
+	public  Transform Pivot;
+	public  float     OpenSpeed = 3;
+	private bool      isOpened = false;
+	private bool      isBroken = false;
 	// TODO : cas où la porte a changé de place
-	private bool  isMoving = false;
+	private bool      isMoving = false;
 
 	private static Quaternion s_closed = Quaternion.Euler(0, 0, 0);
 	private static Quaternion s_opened = Quaternion.Euler(0, 90, 0);
 
 	public void Update() {
 		if(isOpened && transform.eulerAngles.y < 90) {
-			transform.Rotate(Vector3.up, OpenSpeed);
+			transform.RotateAround(Pivot.position, Pivot.up, OpenSpeed);
 			if(transform.eulerAngles.y > 90) {
 				transform.rotation = s_opened;
 			}
 			isMoving = true;
 		}
 		else if(!isOpened && transform.eulerAngles.y > 0) {
-			transform.Rotate(Vector3.down, OpenSpeed);
+			transform.RotateAround(Pivot.position, -Pivot.up, OpenSpeed);
 			if(transform.eulerAngles.y > 90) {
 				transform.rotation = s_closed;
 			}
